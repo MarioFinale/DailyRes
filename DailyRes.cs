981a61ba8974bead10e53987cb766a08495f4c7d
MarioFinale <mariofinale@hotmail.com>
2019-03-17 01:27:18 -0300

    init

---
+++ DailyRes.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using MWBot.net;
+using MWBot.net.WikiBot;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Net;
+
+namespace DailyRes
+{
+    class DailyRes
+    {
+        public static Bot Workerbot { get; private set; }
+
+        public DailyRes(Bot tbot)
+        {
+            Workerbot = tbot;
+        }
+        
+        public Tuple<string,string,string[]> GetResImg(int offset)
+        {
+            Page tpage = Workerbot.Getpage("Plantilla:RDD/º");
+            int tpageindex = int.Parse(tpage.Extract) + offset;
+
+            Page respage = Workerbot.Getpage("Plantilla:RDD/" + tpageindex.ToString());
+            string pageimage = Utils.TextInBetween(respage.Content,"|imagen=", "|")[0].Trim();
+            List<string> links = Utils.TextInBetween(respage.Content, "[[", "]]").ToList();
+
+            for (int i = 0; i < links.Count; i++)
+            {
+                if (links[i].Contains("|"))
+                {
+                    links[i] = links[i].Split('|')[0];
+                }
+                links[i] = "• " + Utils.UppercaseFirstCharacter(links[i]) + ": https://es.wikipedia.org/wiki/" + Utils.UppercaseFirstCharacter(links[i]).Replace(" ", "_");
+            }
+            return new Tuple<string, string, string[]>(respage.Extract, pageimage, links.ToArray());
+        }
+
+        public Tuple<Image, string[]> GetCommonsFile(string CommonsFilename)
+        {
+            string responsestring = Utils.NormalizeUnicodetext(Workerbot.GETQUERY("action=query&format=json&titles=File:" + Utils.UrlWebEncode(CommonsFilename) + "&prop=imageinfo&iiprop=extmetadata|url&iiurlwidth=1000"));
+            string[] thumburlmatches = Utils.TextInBetween(responsestring, "\"thumburl\":\"", "\",");
+            string[] licencematches = Utils.TextInBetween(responsestring, "\"LicenseShortName\":{\"value\":\"", "\",");
+            string[] licenceurlmatches = Utils.TextInBetween(responsestring, "\"LicenseUrl\":{\"value\":\"", "\",");
+            string[] authormatches = Utils.TextInBetween(responsestring, "\"Artist\":{\"value\":\"", "\",");
+            string matchstring = @"<[\S\s]+?>";
+            string matchstring2 = @"\([\S\s]+?\)";
+
+            string licence = string.Empty;
+            string licenceurl = string.Empty;
+            string author = string.Empty;
+
+            if (licencematches.Count() > 0)
+                licence = Regex.Replace(licencematches[0], matchstring, "");
+            if (licenceurlmatches.Count() > 0)
+                licenceurl = Regex.Replace(licenceurlmatches[0], matchstring, "");
+            if (authormatches.Count() > 0)
+            {
+                author = Regex.Replace(authormatches[0], matchstring, "");
+                author = Regex.Replace(author, matchstring2, "").Trim();
+                string[] authors = author.Split(Environment.NewLine.ToCharArray());
+                if (authors.Count() > 1)
+                {
+                    for (int i = 0; i <= authors.Count() - 1; i++)
+                    {
+                        if (!string.IsNullOrWhiteSpace(authors[i]))
+                            author = authors[i];
+                    }
+                }
+                if (author.Contains(":"))
+                    author = author.Split(':')[1].Trim();
+            }
+            Image img = new Bitmap(1, 1);
+            if (thumburlmatches.Count() > 0)
+                img = PicFromUrl(thumburlmatches[0]);
+            if (string.IsNullOrWhiteSpace(author) | (author.ToLower().Contains("unknown")))
+                author = "Desconocido";
+            return new Tuple<Image, string[]>(img, new string[]{licence,licenceurl,author});
+        }
+
+        public Image PicFromUrl(string url)
+        {
+            Image img = new Bitmap(1, 1);
+            try
+            {
+                var request = WebRequest.Create(url);
+                using (var response = request.GetResponse())
+                {
+                    using (var stream = response.GetResponseStream())
+                    {
+                        img = (Image)Image.FromStream(stream).Clone();
+                    }
+                }
+                return img;
+            }
+            catch (Exception ex)
+            {
+                Utils.EventLogger.EX_Log(ex.Message, "DailyRes");
+                img.Dispose();
+                return null;
+            }
+        }
+
+
+    }
+
+
+  
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using MWBot.net.WikiBot;
+using MWBot.net;
+using System.Drawing;
+using DailyRes.Properties;
+
+namespace DailyRes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GlobalVars.Log_Filepath = GlobalVars.Exepath + "dreslog.psv";
+            Bot ESWikiBOT = new Bot(new ConfigFile(GlobalVars.ConfigFilePath));
+            DailyRes tres = new DailyRes(ESWikiBOT);
+            string folderpath = GlobalVars.Exepath + GlobalVars.DirSeparator + "dres" + GlobalVars.DirSeparator;
+
+            for (;;)
+            {
+                try
+                {
+                    if (System.IO.File.Exists(GlobalVars.Exepath + "DailyRes.exe.runme"))
+                    {
+                        System.IO.File.Delete(GlobalVars.Exepath + "DailyRes.exe.runme");
+
+                        if (!System.IO.Directory.Exists(folderpath))
+                        {
+                            System.IO.Directory.CreateDirectory(folderpath);
+                        }
+
+                        for (int i = 0; i < 31; i++)
+                        {
+                            DateTime tday = DateTime.UtcNow.AddDays(i);
+                            string datename = tday.ToString("dd-MM-yyyy");
+
+                            Tuple<string, string, string[]> tx = tres.GetResImg(i);
+                            Tuple<Image, string[]> imgdat = tres.GetCommonsFile(tx.Item2);
+
+                            string tdesc = tx.Item1 + Environment.NewLine + Environment.NewLine;
+                            tdesc = tdesc + tx.Item3[0] + Environment.NewLine + "• Enlace a la imagen completa: https://commons.wikimedia.org/wiki/File:" + Utils.UrlWebEncode(tx.Item2.Replace(" ", "_")) + Environment.NewLine + Environment.NewLine + "• Imagen por " + imgdat.Item2[2];
+                            tdesc = tdesc + Environment.NewLine + "• Licencia: " + imgdat.Item2[0] + " (" + imgdat.Item2[1] + ")";
+                            tdesc = Resources.header + tdesc + Resources.bottom;
+                            System.IO.File.WriteAllText(folderpath + datename + ".htm", tdesc);
+                            System.IO.File.WriteAllText(folderpath + datename + ".commons.htm", "https://commons.wikimedia.org/wiki/File:" + Utils.UrlWebEncode(tx.Item2.Replace(" ", "_")));
+                            imgdat.Item1.Save(folderpath + datename + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                        }
+                        //delete old data
+                        for (int i = -1; i > -4; i = i - 1)
+                        {
+                            DateTime tday = DateTime.UtcNow.AddDays(i);
+                            string datename = tday.ToString("dd-MM-yyyy");
+
+                            if (System.IO.File.Exists(folderpath + datename + ".htm"))
+                            {
+                                System.IO.File.Delete(folderpath + datename + ".htm");
+                            }
+                            if (System.IO.File.Exists(folderpath + datename + ".commons.htm"))
+                            {
+                                System.IO.File.Delete(folderpath + datename + ".commons.htm");
+                            }
+                            if (System.IO.File.Exists(folderpath + datename + ".jpg"))
+                            {
+                                System.IO.File.Delete(folderpath + datename + ".jpg");
+                            }
+                        }                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Utils.EventLogger.EX_Log(ex.Message, "DailyRes");
+                }
+                System.Threading.Thread.Sleep(250);
+            }
+
+        }
+    }
+
+
+
+
+
+
+}
