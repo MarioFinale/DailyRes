2ba9294267ad9d86958941b52a4fba101b6e7db6
MarioFinale <mariofinale@hotmail.com>
2019-03-17 01:18:28 -0300

    Bugfixes

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using MWBot.net.WikiBot;
 using MWBot.net;
 using System.Drawing;
 using DailyRes.Properties;
+using Image = System.Drawing.Image;
 
 namespace DailyRes
 {
@@ -10,19 +11,20 @@ namespace DailyRes
     {
         static void Main(string[] args)
         {
+            string qpath = Utils.Utils.Exepath + "req" + Utils.Utils.DirSeparator;
 
-            GlobalVars.Log_Filepath = GlobalVars.Exepath + "dreslog.psv";
-            Bot ESWikiBOT = new Bot(new ConfigFile(GlobalVars.ConfigFilePath));
+            Utils.Utils.Log_Filepath = Utils.Utils.Exepath + "dreslog.psv";
+            Bot ESWikiBOT = new Bot(new ConfigFile(Utils.Utils.ConfigFilePath), Utils.Utils.Log_Filepath);
             DailyRes tres = new DailyRes(ESWikiBOT);
-            string folderpath = GlobalVars.Exepath + GlobalVars.DirSeparator + "dres" + GlobalVars.DirSeparator;
+            string folderpath = Utils.Utils.Exepath + Utils.Utils.DirSeparator + "dres" + Utils.Utils.DirSeparator;
 
             for (;;)
             {
                 try
                 {
-                    if (System.IO.File.Exists(GlobalVars.Exepath + "DailyRes.exe.runme"))
+                    if (System.IO.File.Exists(qpath + "DailyRes.runme"))
                     {
-                        System.IO.File.Delete(GlobalVars.Exepath + "DailyRes.exe.runme");
+                        System.IO.File.Delete(qpath + "DailyRes.runme");
 
                         if (!System.IO.Directory.Exists(folderpath))
                         {
@@ -35,14 +37,14 @@ namespace DailyRes
                             string datename = tday.ToString("dd-MM-yyyy");
 
                             Tuple<string, string, string[]> tx = tres.GetResImg(i);
-                            Tuple<Image, string[]> imgdat = tres.GetCommonsFile(tx.Item2);
+                            Tuple<System.Drawing.Image, string[]> imgdat = tres.GetCommonsFile(tx.Item2);
 
                             string tdesc = tx.Item1 + Environment.NewLine + Environment.NewLine;
-                            tdesc = tdesc + tx.Item3[0] + Environment.NewLine + "• Enlace a la imagen completa: https://commons.wikimedia.org/wiki/File:" + Utils.UrlWebEncode(tx.Item2.Replace(" ", "_")) + Environment.NewLine + Environment.NewLine + "• Imagen por " + imgdat.Item2[2];
+                            tdesc = tdesc + tx.Item3[0] + Environment.NewLine + "• Enlace a la imagen completa: https://commons.wikimedia.org/wiki/File:" + Utils.Utils.UrlWebEncode(tx.Item2.Replace(" ", "_")) + Environment.NewLine + Environment.NewLine + "• Imagen por " + imgdat.Item2[2];
                             tdesc = tdesc + Environment.NewLine + "• Licencia: " + imgdat.Item2[0] + " (" + imgdat.Item2[1] + ")";
                             tdesc = Resources.header + tdesc + Resources.bottom;
                             System.IO.File.WriteAllText(folderpath + datename + ".htm", tdesc);
-                            System.IO.File.WriteAllText(folderpath + datename + ".commons.htm", "https://commons.wikimedia.org/wiki/File:" + Utils.UrlWebEncode(tx.Item2.Replace(" ", "_")));
+                            System.IO.File.WriteAllText(folderpath + datename + ".commons.htm", "https://commons.wikimedia.org/wiki/File:" + Utils.Utils.UrlWebEncode(tx.Item2.Replace(" ", "_")));
                             imgdat.Item1.Save(folderpath + datename + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
 
                         }
@@ -69,7 +71,7 @@ namespace DailyRes
                 }
                 catch (Exception ex)
                 {
-                    Utils.EventLogger.EX_Log(ex.Message, "DailyRes");
+                    Utils.Utils.EventLogger.EX_Log(ex.Message, "DailyRes");
                 }
                 System.Threading.Thread.Sleep(250);
             }
